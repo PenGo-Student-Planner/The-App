8c4705a8c099352f55a536a48ab25e158b019c6a
Mozzius <mozzius.iii@gmail.com>
2017-05-22 22:00:51 +0100

    Updated tabs

--- pengo/main.js
+++ pengo/main.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 
-class MyHomeScreen extends React.Component {
+class HomeScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: 'Home',
     // Note: By default the icon is only shown on iOS. Search the showIcon option below.
@@ -27,7 +27,7 @@ class MyHomeScreen extends React.Component {
   }
 }
 
-class MyNotificationsScreen extends React.Component {
+class TaskScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: 'Notifications',
     tabBarIcon: ({ tintColor }) => (
@@ -65,32 +65,23 @@ const styles = StyleSheet.create({
 
 const Pengo = TabNavigator({
   Home: {
-    screen: MyHomeScreen,
+    screen: HomeScreen,
   },
-  Notifications: {
-    screen: MyNotificationsScreen,
+  Tasks: {
+    screen: TaskScreen,
   },
 }, {
   tabBarOptions: {
     activeTintColor: '#000',
-<<<<<<< HEAD
     inactiveTintColor: '#333',
     pressColor: '#222',
     style: {
       backgroundColor: '#fff',
     }
-=======
-    activeBackgroundColor: '#fff',
-    inactiveTintColor: '#333',
-    inactiveBackgroundColor: '#eee',
->>>>>>> 4cebb0f108bb497da7772c6377a95cd98ca4f400
   },
   tabBarPosition: 'bottom',
   swipeEnabled: true,
 });
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 4cebb0f108bb497da7772c6377a95cd98ca4f400
 Expo.registerRootComponent(Pengo);
