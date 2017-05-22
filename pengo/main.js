06cb1113704965fb10223db2a65f7134bb9b942a
Mozzius <mozzius.iii@gmail.com>
2017-05-22 21:56:41 +0100

    Merge pull request #1 from PenGo-Student-Planner/initial-branch
    initial commit

---
+++ pengo/main.js
@@ -0,0 +1,87 @@
+import Expo, { Constants } from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+class MyHomeScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Home',
+    // Note: By default the icon is only shown on iOS. Search the showIcon option below.
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./assets/doggo.jpg')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Button
+          onPress={() => this.props.navigation.navigate('Notifications')}
+          title="Go to notifications"
+        />
+      </View>
+    );
+  }
+}
+
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Notifications',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./assets/doggo.jpg')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Go back home"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+  },
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
+
+const Pengo = TabNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: '#000',
+    inactiveTintColor: '#333',
+    pressColor: '#222',
+    style: {
+      backgroundColor: '#fff',
+    }
+  },
+  tabBarPosition: 'bottom',
+  swipeEnabled: true,
+});
+
+
+Expo.registerRootComponent(Pengo);
